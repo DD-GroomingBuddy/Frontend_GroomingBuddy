db334b679457ffde62cf8336d460c0014ca2a455
dsoldatic <soldaticdavid2@gmail.com>
2024-06-02 18:32:21 +0200

    login i signup.vue

--- groomingbuddy/src/components/DogoSignup.vue
+++ groomingbuddy/src/components/DogoSignup.vue
@@ -1,16 +1,174 @@
 <template>
-    <div class="image">
-      <slot></slot>
+  <div class="container-fluid">
+  <div class="row">
+    <div class="col-sm-6 text-black">
+      <div class="px-5 ms-xl-4">
+        <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>
+        <span class="h1 fw-bold mb-0"></span>
+      </div>
+
+      <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
+        <form style="width: 23rem;" @submit.prevent="handleRegister" ref="form">
+          <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 5px;">SIGN UP</h3>
+
+          <div class="form-outline mb-3">
+            <input
+          type="text"
+          class="form-control"
+          name="username"
+          v-model="username"
+          @input="onChangeUsername"
+        />
+            <label class="form-label" for="form2Example18">Username</label>
+          </div>
+
+          <div class="form-outline mb-3">
+            <input
+          type="text"
+          class="form-control"
+          name="email"
+          v-model="email"
+          @input="onChangeEmail"
+        />
+            <label class="form-label" for="form2Example18">Email address</label>
+          </div>
+
+          <!-- <div class="form-outline mb-3">
+            <input type="email" id="form2Example18" class="form-control form-control-lg" v-model="number" />
+            <label class="form-label" for="form2Example18">Phone number</label>
+          </div> -->
+
+          <div class="form-outline mb-3">
+            <input
+          type="password"
+          class="form-control"
+          name="password"
+          v-model="password"
+          @input="onChangePassword"
+        />
+            <label class="form-label" for="form2Example28">Password</label>
+          </div>
+          
+
+          <div class="pt-1 mb-4">
+            <button type="submit" class="btn btn-primary btn-block">Sign Up</button>
+          </div>
+
+          <div v-if="message" class="form-group">
+            <div :class="successful ? 'alert alert-success' : 'alert alert-danger'" role="alert">
+              {{ message }}
+            </div>
+          </div>
+
+          <p class="small mb-5 pb-lg-2"><a class="text-muted" href="#!"></a></p>
+          <router-link to="/login" v-slot="{ navigate }">
+            <p>Already have an account? <a href="#!" class="link-info">Login</a></p>
+          </router-link>
+        </form>
+      </div>
     </div>
-  </template>
-  
-  <style scoped>
-  .image {
-    background-image: url('../assets/dogosignup.png');
-    background-size: 800px 600px; 
-    background-position: center;
-    background-repeat: no-repeat;
-    height: 100vh;
-  }
-  </style>
-  
+
+    <div class="col-sm-6 px-0 d-none d-sm-block">
+      <img
+        src="@/assets/dogosignup.png"
+        alt="Login image"
+        class="w-70 vh-70"
+        style="object-fit: cover; object-position: bottom; margin-top: 230px;">
+    </div>
+  </div>
+</div>
+<!-- <div class="col-md-12">
+<div class="card card-container">
+
+  <form @submit.prevent="handleRegister" ref="form">
+    <div>
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input
+          type="text"
+          class="form-control"
+          name="username"
+          v-model="username"
+          @input="onChangeUsername"
+        />
+      </div>
+
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input
+          type="text"
+          class="form-control"
+          name="email"
+          v-model="email"
+          @input="onChangeEmail"
+        />
+      </div>
+
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input
+          type="password"
+          class="form-control"
+          name="password"
+          v-model="password"
+          @input="onChangePassword"
+        />
+      </div>
+
+      <div class="form-group">
+        <button type="submit" class="btn btn-primary btn-block">Sign Up</button>
+      </div>
+    </div>
+
+    <div v-if="message" class="form-group">
+      <div :class="successful ? 'alert alert-success' : 'alert alert-danger'" role="alert">
+        {{ message }}
+      </div>
+    </div>
+  </form>
+</div>
+</div> -->
+</template>
+
+<script>
+import AuthService from "../services/auth.service";
+
+export default {
+data() {
+return {
+  username: "",
+  email: "",
+  password: "",
+  successful: false,
+  message: "",
+};
+},
+methods: {
+onChangeUsername(e) {
+  this.username = e.target.value;
+},
+onChangeEmail(e) {
+  this.email = e.target.value;
+},
+onChangePassword(e) {
+  this.password = e.target.value;
+},
+handleRegister() {
+  AuthService.register(this.username, this.email, this.password)
+    .then(response => {
+      this.message = response.data.message;
+      this.successful = true;
+    })
+    .catch(error => {
+      const resMessage =
+        (error.response && error.response.data && error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      this.message = resMessage;
+      this.successful = false;
+    });
+},
+},
+};
+</script>
--- groomingbuddy/src/components/GroomingBuddyTitle.vue
+++ groomingbuddy/src/components/GroomingBuddyTitle.vue
@@ -1,72 +1,71 @@
 <template>
-    <div>
-      <h1 class="grooming-buddy-title">Grooming <span class="buddy-text">Buddy</span></h1>
-      <router-link to="/appointments" v-slot="{ navigate }">
-        <button class="appointment-button" @click="navigate"><span class="plus-text">+</span> Appointment</button>
-    </router-link>
-      
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'GroomingBuddy',
-  };
-  </script>
-  
-  <style scoped>
-  .grooming-buddy-title {
-    font-family: 'Roboto', Helvetica, sans-serif;
-    font-size: 150px;
-    font-weight: bold;
-    margin: 0;
-    display: flex;
-    align-items: center;
-    color: white; 
-    margin-left: 100px;
-    padding-top: 110px;
-  }
-  
-  .buddy-text {
-    color: #3a99ce; 
-  }
-  
-  .appointment-button {
-  background-color: #3a99ce;
-  color: white;
-  font-size: 66px;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 10px;
-  margin-left: -65%; 
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  cursor: pointer;
+  <div>
+    <h1 class="grooming-buddy-title">Grooming <span class="buddy-text">Buddy</span></h1>
+    <router-link to="/appointments" v-slot="{ navigate }">
+      <button class="appointment-button" @click="navigate"><span class="plus-text">+</span> Appointment</button>
+  </router-link>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GroomingBuddy',
+};
+</script>
+
+<style scoped>
+.grooming-buddy-title {
+  font-family: 'Roboto', Helvetica, sans-serif;
+  font-size: 150px;
+  font-weight: bold;
+  margin: 0;
+  display: flex;
+  align-items: center;
+  color: white; 
+  margin-left: 100px;
+  padding-top: 110px;
+}
+
+.buddy-text {
+  color: #3a99ce; 
+}
+
+.appointment-button {
+background-color: #3a99ce;
+color: white;
+font-size: 66px;
+padding: 10px 20px;
+border: none;
+border-radius: 10px;
+margin-left: -65%; 
+box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+cursor: pointer;
 }
 
 .appointment-button:hover {
-  background-color: #2c7bb6; 
+background-color: #2c7bb6; 
 }
 
 .plus-text {
-    font-weight: bolder;
+  font-weight: bolder;
 }
 
 @media (max-width: 1600px) {
-  .appointment-button {
-    margin-left: -50%;
-  }
+.appointment-button {
+  margin-left: -50%;
+}
 }
 
 @media (max-width: 1200px) {
-  .appointment-button {
-    margin-left: -40%;
-  }
+.appointment-button {
+  margin-left: -40%;
+}
 }
 
 @media (max-width: 800px) {
-  .appointment-button {
-    margin-left: -30%;
-  }
+.appointment-button {
+  margin-left: -30%;
 }
-  </style>
-  
+}
+</style>
