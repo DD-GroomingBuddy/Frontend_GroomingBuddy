b7b55d61d9b1a6f324282e39069fe43d692a8ba5
dsoldatic <soldaticdavid2@gmail.com>
2024-06-09 21:51:38 +0200

    fix

--- groomingbuddy/src/services/auth.service.js
+++ groomingbuddy/src/services/auth.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth.header";
 
-const API_URL = "https://groomingbuddy.netlify.app";
+const API_URL = "https://groomingbuddy.netlify.app/";
 
 const register = (username, email, password) => {
   return axios.post(API_URL + "signup", {
