bcab957a21bb563b632260865a5b7ce1010b8332
Dino175 <dinolukic56@gmail.com>
2024-09-13 00:31:09 +0200

    userouter

--- groomingbuddy/src/components/AppointmentCard.vue
+++ groomingbuddy/src/components/AppointmentCard.vue
@@ -1,108 +1,108 @@
 <template>
-    <div class="appointment-card">
-      <h3>Appointment Details</h3>
-      <p><strong>User:</strong> {{ appointment.user.username }}</p>
-      <p><strong>Phone Number:</strong> {{ appointment.phoneNumber }}</p>
-      <p><strong>Service:</strong> {{ appointment.service }}</p>
-      <p><strong>Date:</strong> {{ formatDate(appointment.dateTime) }}</p>
-      <p><strong>Time:</strong> {{ formatTime(appointment.dateTime) }}</p>
-      <p><strong>Status:</strong> {{ appointment.status }}</p>
+  <div class="appointment-card">
+    <h3>Appointment Details</h3>
+    <p><strong>User:</strong> {{ appointment.user.username }}</p>
+    <p><strong>Phone Number:</strong> {{ appointment.phoneNumber }}</p>
+    <p><strong>Service:</strong> {{ appointment.service }}</p>
+    <p><strong>Date:</strong> {{ formatDate(appointment.dateTime) }}</p>
+    <p><strong>Time:</strong> {{ formatTime(appointment.dateTime) }}</p>
+    <p><strong>Status:</strong> {{ appointment.status }}</p>
 
-      <!-- Delete button, shown only if the user is an admin -->
-      <button @click="deleteAppointment" v-if="isAdmin" class="btn btn-danger">Delete</button>
-
-      <!-- Update status button, shown only if the appointment is not yet completed -->
+    <!-- Delete button, shown only if the user is an admin -->
+    <button @click="deleteAppointment" v-if="isAdmin" class="btn btn-danger">Delete</button>
+    
+    <!-- Update status button, shown only if the appointment is not yet completed -->
     <button @click="updateStatusToCompleted" v-if="isAdmin && appointment.status !== 'Completed'" class="btn btn-success">Mark as Completed</button>
-    </div>
-  </template>
-  
-  <script>
-  import AuthService from '../services/auth.service';
-  
-  export default {
-    name: "AppointmentCard",
-    props: {
-      appointment: {
-        type: Object,
-        required: true,
-        validator(value) {
-          return (
-            value.hasOwnProperty("user") &&
-            value.hasOwnProperty("phoneNumber") &&
-            value.hasOwnProperty("service") &&
-            value.hasOwnProperty("dateTime") // Ensure dateTime property is present
-          );
-        }
-      }
-    },
-    data(){
-      return {
-        currentUser: AuthService.getCurrentUser()
-      }
-    },
-    computed: {
-      isAdmin() {
-        return this.currentUser.roles.includes('ROLE_ADMIN');
-      }
-    },
-    methods: {
-      formatDate(dateTime) {
-        const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        return new Date(dateTime).toLocaleDateString(undefined, options);
-      },
-      formatTime(dateTime) {
-        const options = { hour: '2-digit', minute: '2-digit' };
-        return new Date(dateTime).toLocaleTimeString(undefined, options);
-      },
-      async deleteAppointment() {
-        // Check if appointment's date and time are in the future
-        const appointmentDateTime = new Date(this.appointment.dateTime);
-        const now = new Date();
-        if (appointmentDateTime <= now) {
-          window.alert("Cannot delete past appointments.");
-          return;
-        }
-        
-        try {
-          await AuthService.deleteAppointment(this.appointment._id);
-          // Emit an event to notify the parent component that an appointment was deleted
-          this.$emit('appointment-deleted', this.appointment._id);
-          window.location.reload()
-        } catch (error) {
-          console.error('Error deleting appointment:', error);
-        }
-      },
-      async updateStatusToCompleted() {
-        try {
-          await AuthService.updateAppointmentStatus(this.appointment._id);
-          this.appointment.status = 'Completed';
-          window.location.reload();
-        } catch (error) {
-          console.error('Error updating appointment status: ', error);
-        }
+  </div>
+</template>
+
+<script>
+import AuthService from '../services/auth.service';
+
+export default {
+  name: "AppointmentCard",
+  props: {
+    appointment: {
+      type: Object,
+      required: true,
+      validator(value) {
+        return (
+          value.hasOwnProperty("user") &&
+          value.hasOwnProperty("phoneNumber") &&
+          value.hasOwnProperty("service") &&
+          value.hasOwnProperty("dateTime") // Ensure dateTime property is present
+        );
       }
     }
-  };
-  </script>
-  
-  <style scoped>
-  .appointment-card {
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    padding: 1em;
-    margin: 1em 0;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  },
+  data(){
+    return {
+      currentUser: AuthService.getCurrentUser()
+    }
+  },
+  computed: {
+    isAdmin() {
+      return this.currentUser.roles.includes('ROLE_ADMIN');
+    }
+  },
+  methods: {
+    formatDate(dateTime) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' };
+      return new Date(dateTime).toLocaleDateString(undefined, options);
+    },
+    formatTime(dateTime) {
+      const options = { hour: '2-digit', minute: '2-digit' };
+      return new Date(dateTime).toLocaleTimeString(undefined, options);
+    },
+    async deleteAppointment() {
+      // Check if appointment's date and time are in the future
+      const appointmentDateTime = new Date(this.appointment.dateTime);
+      const now = new Date();
+      if (appointmentDateTime <= now) {
+        window.alert("Cannot delete past appointments.");
+        return;
+      }
+      
+      try {
+        await AuthService.deleteAppointment(this.appointment._id);
+        // Emit an event to notify the parent component that an appointment was deleted
+        this.$emit('appointment-deleted', this.appointment._id);
+        window.location.reload()
+      } catch (error) {
+        console.error('Error deleting appointment:', error);
+      }
+    },
+    async updateStatusToCompleted() {
+      try {
+        await AuthService.updateAppointmentStatus(this.appointment._id);
+        this.appointment.status = 'Completed'; // Update the local state
+        window.location.reload();
+      } catch (error) {
+        console.error('Error updating appointment status:', error);
+      }
+    }
   }
-  
-  h3 {
-    margin-bottom: 0.5em;
-  }
-  
-  p {
-    margin: 0.5em 0;
-  }
-  
-  strong {
-    font-weight: bold;
-  }
-  </style>
+};
+</script>
+
+<style scoped>
+.appointment-card {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 1em;
+  margin: 1em 0;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+}
+
+h3 {
+  margin-bottom: 0.5em;
+}
+
+p {
+  margin: 0.5em 0;
+}
+
+strong {
+  font-weight: bold;
+}
+</style>
--- groomingbuddy/src/components/DogoLogin.vue
+++ groomingbuddy/src/components/DogoLogin.vue
@@ -1,111 +1,110 @@
 <template>
-    <section class="vh-100">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-sm-6 text-black">
-            <div class="px-5 ms-xl-4">
-              <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>
-              <span class="h1 fw-bold mb-0"></span>
-            </div>
-  
-            <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
-              <form style="width: 23rem;" @submit.prevent="handleLogin" ref="form">
-                <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">LOGIN</h3>
-  
-                <div class="form-outline mb-4">
-                  <input
-                    type="text"
-                    class="form-control"
-                    name="username"
-                    v-model="username"
-                    @input="onChangeUsername"
-                  />
-                  <label class="form-label" for="form2Example18">Username</label>
+  <section class="vh-100">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-6 text-black">
+          <div class="px-5 ms-xl-4">
+            <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>
+            <span class="h1 fw-bold mb-0"></span>
+          </div>
+
+          <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
+            <form style="width: 23rem;" @submit.prevent="handleLogin" ref="form">
+              <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">LOGIN</h3>
+
+              <div class="form-outline mb-4">
+                <input
+                  type="text"
+                  class="form-control"
+                  name="username"
+                  v-model="username"
+                  @input="onChangeUsername"
+                />
+                <label class="form-label" for="form2Example18">Username</label>
+              </div>
+
+              <div class="form-outline mb-4">
+                <input
+                  type="password"
+                  class="form-control"
+                  name="password"
+                  v-model="password"
+                  @input="onChangePassword"
+                />
+                <label class="form-label" for="form2Example28">Password</label>
+              </div>
+
+              <div class="pt-1 mb-4">
+                <button type="submit" class="btn btn-primary btn-block" :disabled="loading">
+                  <span v-if="loading" class="spinner-border spinner-border-sm"></span>
+                  <span>Login</span>
+                </button>
+              </div>
+
+              <div v-if="message" class="form-group">
+                <div class="alert alert-danger" role="alert">
+                  {{ message }}
                 </div>
-  
-                <div class="form-outline mb-4">
-                  <input
-                    type="password"
-                    class="form-control"
-                    name="password"
-                    v-model="password"
-                    @input="onChangePassword"
-                  />
-                  <label class="form-label" for="form2Example28">Password</label>
-                </div>
-  
-                <div class="pt-1 mb-4">
-                  <button type="submit" class="btn btn-primary btn-block" :disabled="loading">
-                    <span v-if="loading" class="spinner-border spinner-border-sm"></span>
-                    <span>Login</span>
-                  </button>
-                </div>
-  
-                <div v-if="message" class="form-group">
-                  <div class="alert alert-danger" role="alert">
-                    {{ message }}
-                  </div>
-                </div>
-  
-                <p class="small mb-5 pb-lg-2"><a class="text-muted"></a></p>
-                <router-link to="/signup" v-slot="{ navigate }">
-                  <p><a class="link-info">Register here</a></p>
-                </router-link>
-              </form>
-            </div>
+              </div>
+
+              <p class="small mb-5 pb-lg-2"><a class="text-muted"></a></p>
+              <router-link to="/signup" v-slot="{ navigate }">
+                <p><a class="link-info">Register here</a></p>
+              </router-link>
+            </form>
           </div>
         </div>
       </div>
-    </section>
-  </template>
-  
-  <script>
-  import { ref } from 'vue'; // Ensure ref is imported from vue
-  import { useRouter } from 'vue-router';
-  import AuthService from '../services/auth.service';
-  
-  export default {
-    setup() {
-      const form = ref(null);
-      const username = ref("");
-      const password = ref("");
-      const loading = ref(false);
-      const message = ref("");
-      const navigate = useRouter();
-  
-      const onChangeUsername = (e) => {
-        username.value = e.target.value;
-      };
-  
-      const onChangePassword = (e) => {
-        password.value = e.target.value;
-      };
-  
-      const handleLogin = (e) => {
-        e.preventDefault();
-  
-        message.value = "";
-        loading.value = true;
-  
-        AuthService.login(username.value, password.value)
-          .then(() => {
-            navigate.push({name: "home", replace: true}).then(() =>{
-              window.location.reload();
-            });
-          })
-          .catch((error) => {
-            const resMessage =
-              (error.response && error.response.data && error.response.data.message) ||
-              error.message ||
-              error.toString();
-  
-            loading.value = false;
-            message.value = resMessage;
-          });
-      };
-  
-      return { form, username, password, loading, message, onChangeUsername, onChangePassword, handleLogin };
-    },
-  };
-  </script>
-  
+    </div>
+  </section>
+</template>
+
+<script>
+import { ref } from 'vue'; // Ensure ref is imported from vue
+import { useRouter } from 'vue-router';
+import AuthService from '../services/auth.service';
+
+export default {
+  setup() {
+    const form = ref(null);
+    const username = ref("");
+    const password = ref("");
+    const loading = ref(false);
+    const message = ref("");
+    const router = useRouter();
+
+    const onChangeUsername = (e) => {
+      username.value = e.target.value;
+    };
+
+    const onChangePassword = (e) => {
+      password.value = e.target.value;
+    };
+
+    const handleLogin = (e) => {
+      e.preventDefault();
+
+      message.value = "";
+      loading.value = true;
+
+      AuthService.login(username.value, password.value)
+        .then(() => {
+          router.push({ name: "home", replace: true }).then(() => {
+            window.location.reload(); // Reload the page after navigation
+          });      
+        })
+        .catch((error) => {
+          const resMessage =
+            (error.response && error.response.data && error.response.data.message) ||
+            error.message ||
+            error.toString();
+
+          loading.value = false;
+          message.value = resMessage;
+        });
+    };
+
+    return { form, username, password, loading, message, onChangeUsername, onChangePassword, handleLogin };
+  },
+};
+</script>
